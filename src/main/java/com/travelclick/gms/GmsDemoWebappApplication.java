934971b2a4a2c4b275b8efbfb327fad43d1cf684
Jon Simpson <jon@jonsimpson.ca>
2017-05-06 21:36:40 -0400

    initial commit

---
+++ src/main/java/com/travelclick/gms/GmsDemoWebappApplication.java
@@ -0,0 +1,12 @@
+package com.travelclick.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsDemoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsDemoWebappApplication.class, args);
+	}
+}
